1aa390e6f1f5b0aa0d131fda1ceb63ffd8dea9e8
Richard Mark Haes-Ellis <richard9661h@gmail.com>
2020-04-02 23:45:35 +0200

    Symplified and new algoritm based on iforce2d code

--- DueTimerISR/DueTimerISR.ino
+++ DueTimerISR/DueTimerISR.ino
@@ -36,22 +36,20 @@
 #define RUN 3
 
 // Direction of stepper motor movement
-#define CW 0
+#define CW -1
 #define CCW 1
-
-#define TRUE 1
-#define FALSE 0
 
 typedef struct
 {
-       unsigned char run_state : 3; //! What part of the speed ramp we are in.
        unsigned char dir : 1;       //! Direction stepper motor should move.
        unsigned int step_delay;     //! Peroid of next timer delay. At start this value set the accelration rate c0.
-       unsigned int decel_start;    //! What step_pos to start decelaration
-       signed int decel_val;        //! Sets deceleration rate.
        unsigned int min_delay;      //! Minimum time delay (max speed)
        signed int n;                //! Counter used when accelerateing/decelerateing to calculate step_delay.
-       unsigned char running : TRUE;
+       unsigned char running : true;
+       unsigned int rampUpStepCount;
+       unsigned int total_steps;
+       unsigned int step_count;
+       int          step_position;
 } speedRampData;
 
 speedRampData motor;
@@ -59,8 +57,6 @@ speedRampData motor;
 int pos = 50000;
 int acc = 5;
 int vel = 500;
-
-unsigned int stepsTaken;
 
 void startTimer()
 {
@@ -110,189 +106,81 @@ void configureTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency)
        // NVIC_EnableIRQ(irq);
 }
 
-void speed_cntr_Move(signed int steps, unsigned int accel, unsigned int speed)
+int speed_cntr_Move(signed int steps, unsigned int accel, unsigned int speed)
 {
-       unsigned int steps_2_max_speed; //! Number of steps before we hit max speed.
-       unsigned int steps_until_decel; //! Number of steps before we must start deceleration (if accel does not hit max speed).
-       unsigned int decel = accel;
-
-       if (steps < 0) // Set direction from sign on steps value.
-       {
-              motor.dir = CCW;
-              steps = -steps; // We make it positive
-              digitalWrite(PIN_Z_DIR, HIGH);
+       motor.dir = steps > 0 ? CCW : CW;                // Save the direction state 
+       digitalWrite(PIN_Z_DIR,steps>0?HIGH:LOW);        // Set the direction depending on the steps
+       motor.total_steps = abs(steps);                  // Change the sign of the steps to positive
+       if(accel == 0){                                  // If we get zero acceleration get out.
+              return false;
        }
-       else
-       {
-              motor.dir = CW;
-              digitalWrite(PIN_Z_DIR, LOW);
+       // CALCULATE ACCELERATION C0
+       motor.step_delay = T1_FREQ*sqrt((float)2/accel);        // We save the acceleration to the first step delay. (NEEDS CHANGING)
+       motor.step_count = 0;                            // Initialize the step count to zero 
+       motor.n = 0;                                     // Set the ramp counter to zero.
+       if(speed == 0){                                  // If we input zero speed get out.
+              return false;
        }
+       // CALCULATE SPEED MIN_DELAY
+       motor.min_delay = T1_FREQ / speed;               // Set the minumum delay needed for the speed given
+       motor.rampUpStepCount = 0;                       // Set the ramp counter to zero. Is it not the same as n??
+       motor.running = true;                            // Set the motor status to running
+       TC1->TC_CHANNEL[0].TC_RC = motor.step_delay;     // Set counter ragister to the starting delay (acceleration)
+       digitalWrite(PIN_Z_ENABLE,LOW);                  // Enable stepper motor
+       startTimer();                                    // Start the timer
 
-       if (steps == 1) // If moving only 1 steps.
-       {
-              motor.n = -1;            // Move one steps...
-              motor.run_state = DECEL; // ...in DECEL state.
-              motor.step_delay = 1000; // Just a short delay so main() can act on 'running'.
-              motor.running = TRUE;
+       Serial.println("Min delay:"    +String(motor.min_delay));
+       Serial.println("Start delay:"  +String(motor.step_delay));
+       Serial.println("Total steps:"  +String(motor.total_steps));
 
-              TC1->TC_CHANNEL[0].TC_RC = 10;
-       }
-
-       else if (steps != 0) // Only move if number of steps to move is not zero.
-       {
-
-              motor.min_delay = A_T_x100 / speed;                                               // Minimum delay for a given speed
-              motor.step_delay = (T1_FREQ_148 * sqrt(A_SQ / accel)) / 100;                      // First C0 delay time
-              steps_2_max_speed = (long)(speed * speed) / (long)((long)(A_x20000 * accel) / 100); // steps_2_max_speed = speed^2 / (2*alpha*accel)
-              Serial.println("Motor minimim step delay:"+String(motor.min_delay));
-              Serial.println("Motor initial step delay C0:"+String(motor.step_delay));
-              Serial.println("Motor steps to maximum speed:"+String(steps_2_max_speed));
-
-              if (steps_2_max_speed == 0) // We need at least 1 step to get to any speed.
-              {
-                     steps_2_max_speed = 1;
-              }
-
-              steps_until_decel = (steps * 0.5); // n1 = (n1+n2)decel / (accel + decel)
-
-              if (steps_until_decel == 0) // We must accelrate at least 1 steps before we can start deceleration.
-              {
-                     steps_until_decel = 1;
-              }
-
-              if (steps_until_decel <= steps_2_max_speed) // We wont reach desired speed
-              {
-                     motor.decel_val = steps_until_decel - steps; // n1 - steps  = -n2 ==>> DECEL STEPS
-                     Serial.println("Motor cant reach desired speed");
-              }
-              else // We will reach max speed
-              {
-                     motor.decel_val = -steps_2_max_speed; // n1*accel1 = n2*accel2 to get to --> n2 = -n1*(accel1/decel2)
-                     Serial.println("1:"+String(steps_2_max_speed)+" 2:"+String(accel)+" 3:"+String(decel));
-                     Serial.println("Motor can reach desired speed: "+String(motor.decel_val));
-              }
-
-              if (motor.decel_val == 0) // We must decelrate at least 1 steps to stop.
-              {
-                     motor.decel_val = -1;
-              }
-              Serial.println("Motor deceleration value:"+String(motor.decel_val));
-
-              motor.decel_start = steps + motor.decel_val; // steps - n2 = number of steps befor me start decelerating
-              Serial.println("Deceleration starts at step:"+String(motor.decel_start));
-
-              if (motor.step_delay <= motor.min_delay) // If the maximum speed is so low that we dont need to go via accelration state.
-              {
-                     motor.step_delay = motor.min_delay;
-                     motor.run_state = RUN; // Go straght to run state
-              }
-              else // If step_delay (Initial speed) is larger than minimum delay then we accelerate
-              {
-                     motor.run_state = ACCEL; // Go to accel state
-              }
-
-              motor.n = 0; // Reset acceleration / deceleration counter. Used in the Cn=cn_1-(4*Cn_1+rest)/(4*n+1)
-              motor.running = TRUE;
-              digitalWrite(PIN_Z_ENABLE, LOW);
-
-              TC1->TC_CHANNEL[0].TC_RC = 10; // Doesent really matter because its the time for the first step, after that it would already be recalculated.
-              startTimer();
-              stepsTaken = 0;
-       }
+       Serial.println("Desired position:" +String(steps));
+       Serial.println("Acceleration:"     +String(accel));
+       Serial.println("Max velocity:"     +String(speed));
 }
 
 void TC3_Handler()
 {
        TC_GetStatus(TC1, 0);               // Timer 1 channel 0 ----> TC3 it also clear the flag
-       unsigned int new_step_delay = 0;    // Holds next delay period.
-       static int last_accel_delay;        // Remember the last steps delay used when accelrating.
-       static unsigned int step_count = 0; // Counting steps when moving.
-       static unsigned int rest = 0;       // Keep track of remainder from new_step-delay calculation to incrase accurancy
 
-       switch (motor.run_state)
+       if (motor.step_count < motor.total_steps)
        {
-       case STOP:
-              step_count = 0;
-              stepsTaken = 0;
-              rest = 0;
-              stopTimer();
-              motor.running = FALSE;
-              digitalWrite(PIN_Z_ENABLE, HIGH);
-              break;
-
-       case ACCEL:
-              /////// WE STEP ONCE /////////
               digitalWrite(PIN_Z_STEP, HIGH);
               digitalWrite(PIN_Z_STEP, LOW);
-              step_count++;
-              stepsTaken++;
-              //////////////////////////////
-              motor.n++; // This is the n in the equations
-
-              new_step_delay = motor.step_delay - (((2 * (long)motor.step_delay) + rest) / (4 * motor.n + 1));
-              rest = ((2 * (long)motor.step_delay) + rest) % (4 * motor.n + 1);
-
-       
-              
-              if (step_count >= motor.decel_start)             // Chech if we should start decelration.
-              {
-                     // motor.n = motor.decel_val;             // Is it not the same already??? NO, decel_val is negative.
-                     motor.run_state = DECEL;
-                     rest = 0;
-              } 
-              else if (new_step_delay <= motor.min_delay)      // Chech if we hitted max speed.
-              {
-                     last_accel_delay = new_step_delay;
-                     new_step_delay = motor.min_delay;
-                     rest = 0;
-                     motor.run_state = RUN;
-              }
-              break;
-
-       case RUN:
-              /////// WE STEP ONCE /////////
-              digitalWrite(PIN_Z_STEP, HIGH);
-              digitalWrite(PIN_Z_STEP, LOW);
-              step_count++;
-              stepsTaken++;
-              //////////////////////////////
-
-              new_step_delay = motor.min_delay; // I think its not neccesary
-              // Chech if we should start decelration.
-              if (step_count >= motor.decel_start)
-              {
-                     // motor.n = motor.decel_val;
-                     rest = 0;
-                     // Start decelration with same delay as accel ended with.
-                     new_step_delay = last_accel_delay;
-                     motor.run_state = DECEL;
-              }
-              break;
-
-       case DECEL:
-              /////// WE STEP ONCE /////////
-              digitalWrite(PIN_Z_STEP, HIGH);
-              digitalWrite(PIN_Z_STEP, LOW);
-              step_count++;
-              stepsTaken++;
-              //////////////////////////////
-              
-              motor.n--; // Starts out negative and slowarly goes positive.
-              new_step_delay = (long)motor.step_delay*((4 * motor.n + 1 + rest)/(4 * motor.n + 1 - 2));
-              rest = (4 * motor.n + 1 + rest) % (4 * motor.n + 1 - 2);
-              // Check if we at last steps
-              if (motor.n <= 0)
-              {
-                     motor.run_state = STOP;
-                     rest = 0;
-                     stopTimer();
-                     motor.running = FALSE;
-                     digitalWrite(PIN_Z_ENABLE, HIGH);
-              }
-              break;
+              motor.step_count++;
+              motor.step_position += motor.dir;
        }
-       motor.step_delay = new_step_delay;
-       TC1->TC_CHANNEL[0].TC_RC = motor.step_delay; // Set counter limit in the timer counter register
+       else // If we step more that the total it means we have already finished
+       {
+              motor.running = false;
+              digitalWrite(PIN_Z_ENABLE,HIGH);
+              stopTimer();
+       }
+
+       if (motor.rampUpStepCount == 0) // If we are ramping up 
+       { 
+              // Calculate next delays 
+              motor.n++;    
+              motor.step_delay = motor.step_delay - (2 * motor.step_delay) / (4 * motor.n + 1);
+
+              // If we reach max speed by checkig if the calculated delay is smaller that the one we calculated with the max speed
+              if (motor.step_delay <=  motor.min_delay)
+              { 
+                     motor.step_delay = motor.min_delay;       // We saturate it to that minimum delay 
+                     motor.rampUpStepCount = motor.step_count; // We save the number of steps it took to reach this speed
+              }
+              // If istead we manage to reach half way without reaching full speed we have to start decelerating 
+              if (motor.step_count >= motor.total_steps / 2)
+              { 
+                     motor.rampUpStepCount = motor.step_count; // So we save the number of steps it took to accelerate to this speed 
+              }
+       }
+       // If we dont have to ramp down yet (we shoudl be in eather run or acelerating)
+       else if (motor.step_count >= motor.total_steps - motor.rampUpStepCount)
+       { 
+              motor.n--;
+              motor.step_delay = ( motor.step_delay * (4 * motor.n + 1)) / (4 * motor.n + 1 - 2); // THis is the same as the other equation but inverted
+       }
+       TC1->TC_CHANNEL[0].TC_RC = motor.step_delay;
 }
 
 void setup()
@@ -308,23 +196,13 @@ void setup()
 
 void loop()
 {
-       Serial.println("A_T_x100:"         +String(A_T_x100));
-       Serial.println("ALPHA:"            +String(ALPHA));
-       Serial.println("T1_FREQ_148:"      +String(T1_FREQ_148));
-       Serial.println("A_SQ:"             +String(A_SQ));
-       Serial.println("A_x20000:"         +String(A_x20000));
-
-       Serial.println("Desired position:" +String(pos));
-       Serial.println("Acceleration:"     +String(acc));
-       Serial.println("Max velocity:"     +String(vel));
-
        speed_cntr_Move(pos, acc, vel);
        while (motor.running)
        {
-              Serial.println(" n:"            + String(motor.n)           +
-                             " run_state:"    + String(motor.run_state)   +
-                             " running:"      + String(motor.running)     +
-                             " step:"         + String(stepsTaken)        +
+              Serial.println(" n:"            + String(motor.n)              +
+                             " step:"         + String(motor.step_count)     +
+                             " running:"      + String(motor.running)        +
+                             " step_pos:"     + String(motor.step_position)  +
                              " step_delay:"   + String(motor.step_delay));
        }
        Serial.println("Finished movement");
