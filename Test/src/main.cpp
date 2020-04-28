f648ec492371de3026c6c41fb02f3eb9e63d9af1
Richard Mark Haes-Ellis <richard9661h@gmail.com>
2020-04-28 19:10:52 +0200

    Functioning comunication between due and pc

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,32 +1,144 @@
 #include <Arduino.h>
 
-uint32_t data[3];
+// #define debug
 
+union payload {
 
-union u_tag{
-  uint32_t number;
+  int32_t numbers[4];
+  uint8_t array[12];
+
+} m_payload;
+
+union integer {
+
+  int32_t number;
   uint8_t array[4];
-}m_data;
 
-void setup() {
+} m_integer;
+
+enum protocolState
+{
+  LISTENING, // blink disable
+  READ_LOAD, // blink enable
+  READ_END,  // we want the led to be on for interval
+  END_CMD    // we want the led to be off for interval
+};
+
+void setup()
+{
   Serial.begin(9600);
-  SerialUSB.begin(9600); 
+  SerialUSB.begin(9600);
   delay(3000);
-  SerialUSB.println("Ready");
+  SerialUSB.print("\nReady");
 }
 
-void loop() {
-  // while(!Serial.available()){} // If there is no data do nothing 
-  // We do something when we recieve somthing
-  if (Serial.available()){
-    if(Serial.read()=='\n'){
-      for(uint8_t j=0;j<2;j++){
-        for(uint8_t i=0;i<3;i++){
-          m_data.array[i] = Serial.read();
+void loop()
+{
+
+  if (Serial.available())
+  {
+#ifdef debug
+    SerialUSB.println("\nIncomming data:");
+#endif
+
+    int16_t byten = 11;
+    uint8_t state = LISTENING;
+    uint8_t new_packet = 0;
+    do
+    {
+      while (Serial.available() == 0)
+        ;                              // Wait for incomming data // ADD TIMEOUT GOD DAMN IT
+      uint8_t in_byte = Serial.read(); // READ BYTE
+#ifdef debug
+      SerialUSB.print("State: " + String(state) + " Byte: ");
+      SerialUSB.print(in_byte, HEX);
+      SerialUSB.println();
+#endif
+      switch (state)
+      {
+      case LISTENING:
+        if (in_byte == 0x7E) // CHECK IF ITS START BYTE
+        {
+#ifdef debug
+          SerialUSB.println("\n START command read");
+#endif
+
+          state = READ_LOAD;
         }
-        data[j]=m_data.number;
-      }   
+        else
+        {
+// Wrong start command
+#ifdef debug
+          SerialUSB.println("\nWrong START BYTE");
+#endif
+
+          state = END_CMD; // RESET IF IT ISN'T
+        }
+        break;
+
+      case READ_LOAD:
+        // Serial.readBytes(m_payload.array,12);
+        m_payload.array[byten] = in_byte;
+        byten--;
+        if (byten < 0)
+        {
+#ifdef debug
+          SerialUSB.println("\nRecived 12 bytes");
+#endif
+
+          // for(int i=0;i<12;i++)
+          //   SerialUSB.print(m_payload.array[i],HEX);
+          state = READ_END;
+          byten = 3;
+        }
+        break;
+
+      case READ_END:
+        // Serial.readBytes(m_integer.array,4);
+        m_integer.array[byten] = in_byte;
+        byten--;
+        if (byten < 0)
+        {
+#ifdef debug
+          SerialUSB.println("\nRecived end command");
+          for (int i = 0; i < 4; i++)
+            SerialUSB.print(m_integer.array[i], HEX);
+          SerialUSB.println();
+          SerialUSB.println(m_integer.number, HEX);
+#endif
+
+          if (m_integer.number == (int32_t)0x7FFFFFFF) // END COMMAND
+          {
+            // Data is correct
+            new_packet = 1;
+            state = END_CMD;
+#ifdef debug
+            SerialUSB.println("\nEnd packet");
+#endif
+          }
+          else
+          {
+#ifdef debug
+            SerialUSB.println("\nWrong packet");
+#endif
+
+            state = END_CMD;
+          }
+        }
+        break;
+      }
+    } while (state != END_CMD);
+
+    if (new_packet)
+    {
+      new_packet = 0;
+      SerialUSB.println();
+      SerialUSB.print("Recieved M1 = ");
+      SerialUSB.print(m_payload.numbers[2]);
+      SerialUSB.print(" M2 = ");
+      SerialUSB.print(m_payload.numbers[1]);
+      SerialUSB.print(" M3 = ");
+      SerialUSB.println(m_payload.numbers[0]);
     }
-    SerialUSB.println("Recieved M1 = "+String(data[0])+" M2 = "+String(data[1])+" M3 = "+String(data[2]));
   }
 }
--- python/test2.py
+++ python/test2.py
@@ -1,19 +1,29 @@
 
-import json
 import serial
 import time
 import struct
+import binascii
 
-ser = serial.Serial("/dev/ttyACM0",9600)
-time.sleep(5)
+ser = serial.Serial("/dev/ttyACM1",9600)
+time.sleep(3)
 
 
-def packIntegerAsULong(v1,v2,v3):
-    """Packs a python 4 byte unsigned integer to an arduino unsigned long"""
-    return struct.pack('III', v1,v2,v3)    #should check bounds
+a = 100
+b = -50
+c = -500
+
+def sendPacket(v1,v2,v3):
+    """Packs a python 4 byte integer to an arduino unsigned long"""
+    packet = struct.pack('>biiii',126,v1,v2,v3,2147483647) 
+    print(binascii.hexlify(bytearray(packet)))
+    return packet    #should check bounds
 
 while True:
+
+    a = a + 10
+    b = b - 10
+    c = c + 10
+    
     # send and receive via pyserial
-    ser.write(('\n').encode())
-    ser.write(packIntegerAsULong(100,342,534))
-    time.sleep(2)
+    ser.write(sendPacket(a,b,c))
+    # time.sleep(0.01)
